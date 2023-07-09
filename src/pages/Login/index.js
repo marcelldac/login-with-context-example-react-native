6df91ecb072d5bb99ec6dbddaf5432add02c96a8
marcelldac <dacmarcell@proton.me>
2023-07-09 15:50:21 -0300

    feat: adiciona estilizacao styled components

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,55 +1,33 @@
 import React, {useState, useContext} from 'react';
 import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
-import {AuthContext} from '../../contexts/auth'
+import {AuthContext} from '../../contexts/auth';
+import {Content, Input, Button, ButtonText} from './styles.js';
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const {signIn} = useContext(AuthContext)
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const {signIn} = useContext(AuthContext);
 
-  function handleLogin(){
-    signIn(email, password)
-  }
+	function handleLogin(){
+		signIn(email, password);
+	}
 
-  return(
-    <View style={styles.container}>
-      <Text>Seja bem-vindo(a)</Text>
-
-      <TextInput 
-        style={styles.input}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        placeholder='Digite seu e-mail'
-      />
-
-      <TextInput 
-        style={styles.input}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        placeholder='Digite sua senha'
-      />
-
-      <TouchableOpacity onPress={handleLogin} style={styles.button}>
-        <Text style={styles.buttonText}>Acessar</Text>
-      </TouchableOpacity>
-    </View>
-  )
+	return(
+		<Content>
+			<Text>Seja bem-vindo(a)</Text>
+			<Input
+			value={email}
+			onChangeText={(text) => setEmail(text)}
+			placeholder='Digite seu e-mail'
+			/>
+			<Input
+			value={password}
+			onChangeText={(text) => setPassword(text)}
+			placeholder='Digite sua senha'
+			/>
+			<Button onPress={handleLogin}>
+				<ButtonText>Acessar</ButtonText>
+			</Button>
+		</Content>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    borderRadius: 10,
-    backgroundColor: 'gray',
-  },
-  button: {
-    backgroundColor: 'red',
-  },
-  buttonText: {
-    color: 'white',
-  },
-})
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Content = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	font-family: 'monospace';
+`
+
+export const Input = styled.TextInput`
+	background-color: gray;
+	width: 300px;
+	height: 50px;
+	margin-bottom: 15px;
+`
+
+export const Button = styled.TouchableOpacity`
+	background-color: red;
+`
+
+export const ButtonText = styled.Text`
+	color: white;
+`
