fbacb540efe0dbaf02a6f37e438bbd87f54b6a0d
marcelldac <devmarcell@protonmail.com>
2024-06-08 14:31:48 -0300

    refactor: remove home touchable opacity and add shows user profile directly

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import {TouchableOpacity, Text} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import {HomeView} from './styles';
 
+import UserProfile from '../UserProfile/index.js'
+
 export default function Home(){
-	const navigation = useNavigation();
 	return(
 		<HomeView>
-			<Text>teste</Text>
-			<TouchableOpacity onPress={() => {navigation.push("UserProfile")}}>
-				<AntDesign
-				name="user"
-				size={20}
-				/>
-			</TouchableOpacity>
+			<UserProfile />
 		</HomeView>
   )
 }
