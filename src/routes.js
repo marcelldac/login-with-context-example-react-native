178e613dfd0bb6e61298bedf4ab8d61d1cceaa84
marcelldac <devmarcell@protonmail.com>
2024-06-08 11:58:06 -0300

    style: lint

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -6,14 +6,14 @@ import UserProfile from './pages/UserProfile';
 
 const Stack = createNativeStackNavigator();
 
-function Routes(){
-    return(
-        <Stack.Navigator screenOptions={{headerShown:false}}>
-            <Stack.Screen name="Login" component={Login}/>
-            <Stack.Screen name="Home" component={Home}/>
-            <Stack.Screen name="UserProfile" component={UserProfile}/>
-        </Stack.Navigator>
-    )
+function Routes() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="UserProfile" component={UserProfile} />
+    </Stack.Navigator>
+  );
 }
 
 export default Routes;
